f5b5fc4139fa6e1629107be883ea3c960a83f922
Lucas Albuquerque <koppke@MacBook-de-Lucas-Albuquerque.local>
2013-08-28 01:20:06 -0300

    Implementando o metodo de escolha de palavra

--- forca.py
+++ forca.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-                                                        
 import random
+                        
+class Forca():
+
+    def __init__(self):
+        self.lista_de_palavras = []
+        self.palavra_escolhida = ''
                                                                               
-def le_arquivo():                                                              
-    lista_de_palavras = []                                                     
-    for palavra in open('dictionary.txt'):                                     
-        lista_de_palavras.append(palavra[:-2])
-    print lista_de_palavras[:11]
+    def le_arquivo(self):                               
+        for palavra in open('dictionary.txt'):                                     
+            self.lista_de_palavras.append(palavra[:-2])
+        return True    
 
-def define_palavra():
-    pass
+    def define_palavra(self):
+        tamanho_palavra = raw_input('Qual o tamanho de palavra você deseja? ')
+        palavras_possiveis = [palavra for palavra in self.lista_de_palavras if len(palavra) == int(tamanho_palavra)]
+        if len(palavras_possiveis) != 0:
+            self.palavra_escolhida = random.choice(palavras_possiveis)
 
-le_arquivo()
+forca = Forca()
+forca.le_arquivo()
+forca.define_palavra()
