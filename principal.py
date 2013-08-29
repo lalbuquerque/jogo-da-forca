8079292906fbf49596b17e1bb838bdca5ae0d4f9
loliveira <lucas.oliveira@acotel.com>
2013-08-29 18:34:23 -0300

    Alterando o nome do arquivo jogo para principal

---
+++ principal.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from forca import Forca
+from os import system
+
+
+jogo = Forca()
+if jogo.le_arquivo():
+    jogo.define_palavra()
+    jogo.define_qtd_erros()
+    jogo.monta_palavra_escondida()
+    jogo.monta_alfabeto()
+    system('clear')
+    while jogo.qtd_de_erros > 0:
+        print '######################################################'
+        print '###########       JOGO DA FORCA        ###############'
+        print '######################################################'
+        print
+        print
+        jogo.exibe_palavra_escondida()
+        print
+        print
+        jogo.exibe_alfabeto()
+        print
+        print
+        print 'Tamanho da palavra: %s' % jogo.tamanho_palavra
+        print 'Voce tem %s erros restantes ' % jogo.qtd_de_erros
+        jogo.coleta_escolha()
+        system('clear')
+        continue
+        if jogo.qtd_de_erros == 0:
+            break
+    print 'A palavra sorteada era: %s' % jogo.palavra_escolhida
+    print 'GAME OVER !!!!!!!!!!!!!!!!!!!!!!!!!!11'
